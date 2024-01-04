45ddb0afdd7465c46905c1a2bdc524d88f8d8aaf
Robert Angote <ebobby@outlook.com>
2024-01-04 12:47:27 +0300

    Adding the application db context class, essential for DB connection.

---
+++ BulkyWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) 
+        {
+                
+        }
+    }
+}
