4d9c7e20e869527deca4248c840bc1a30d56d5fe
Robert Angote <ebobby@outlook.com>
2024-01-06 15:51:25 +0300

    Model Edit features

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -20,6 +20,21 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot be similar to the Category Name.");
+            }
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+        }
     }
 }
