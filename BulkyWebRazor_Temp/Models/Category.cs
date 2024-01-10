1c778f10005a96cd14e6a997696c70be9c6df42e
Robert Angote <ebobby@outlook.com>
2024-01-10 21:03:50 +0300

    Addition  of the db connection configuration settings.

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key] //Data annotations
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "A valid display order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
