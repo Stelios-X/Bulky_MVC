f5581ea8944f434aeaac20fb88c33e165ca8e632
Robert Angote <ebobby@outlook.com>
2024-02-02 03:51:07 +0300

    Creating get  and post ahndlers and property binding in razor pages.

--- BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -8,6 +8,7 @@ namespace BulkyWebRazor_Temp.Pages.Categories
     public class CreateModel : PageModel
     {
         private readonly ApplicationDBContext _db;
+        [BindProperty]
         public Category Category { get; set; }
         public CreateModel(ApplicationDBContext db)
         {
@@ -17,6 +18,13 @@ namespace BulkyWebRazor_Temp.Pages.Categories
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost() //The return type is informed by the fact that we're returning a page
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
